0230a2d0fed1376af2d493267e5f57665ccb7902
Guto-potato <gustavo.borghi18@gmail.com>
2024-10-03 15:25:52 +0000

    adicionamos codigo js

---
+++ CALCULADORA/js/script.js
@@ -0,0 +1,13 @@
+// FAZ REFERENCIA  AOS ELEMENTOS DOM
+const numero1 = document.querySelector('#numero1')
+const numero2 = document.querySelector('#numero2')
+const btncalcular = document.querySelector('#btn-calcular')
+const resultado = document.querySelector('.resultado')
+const divisao = document.querySelector('.divicao')
+const adicao = document.querySelector('.adicao')
+const subitracao = document.querySelector('.subitracao')
+const multiplicacao = document.querySelector('.multiplicacao')
+// alert(numero1)
+//CRIA FUNÇÃO CALCULAR
+
+//ADICIONA ESCUTAR DE EVENTOS
