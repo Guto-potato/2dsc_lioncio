aa1519404fe5be8e205f45d62b7d8aad5854b86b
Guto-potato <gustavo.borghi18@gmail.com>
2024-10-10 15:29:08 +0000

    Criada a funcao Calcular

--- CALCULADORA/js/script.js
+++ CALCULADORA/js/script.js
@@ -1,13 +1,27 @@
 // FAZ REFERENCIA  AOS ELEMENTOS DOM
 const numero1 = document.querySelector('#numero1')
 const numero2 = document.querySelector('#numero2')
-const btncalcular = document.querySelector('#btn-calcular')
-const resultado = document.querySelector('.resultado')
-const divisao = document.querySelector('.divicao')
-const adicao = document.querySelector('.adicao')
-const subitracao = document.querySelector('.subitracao')
-const multiplicacao = document.querySelector('.multiplicacao')
+const btnCalcular = document.querySelector('#btn-calcular')
+const resultado = document.querySelector('#resultado')
+const divisao = document.querySelector('#divicao')
+const adicao = document.querySelector('#adicao')
+const subitracao = document.querySelector('#subitracao')
+const multiplicacao = document.querySelector('#multiplicacao')
 // alert(numero1)
 //CRIA FUNÇÃO CALCULAR
-
+function calcular(){
+    const n1 = Number (numero1.value)
+    const n2 = Number (numero2.value)
+    if(typeof n1 === 'number'
+        && typeof n2 === 'number') {
+            const adicao = `A soma de ${n1} e ${n2} = ${n1=n2}`
+            alert (adicao)
+        } else {
+            alert ('Por favor, digite um número')
+        }
+    //alert('resultado:  ' + resultado)
+}
 //ADICIONA ESCUTAR DE EVENTOS
+btnCalcular.addEventListener('click', function(){
+   calcular()
+})
