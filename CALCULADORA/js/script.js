c2461322e0311cbb606598cd3ce5d56dc19d9651
Guto-potato <gustavo.borghi18@gmail.com>
2024-10-17 15:21:20 +0000

    funcionamento js

--- CALCULADORA/js/script.js
+++ CALCULADORA/js/script.js
@@ -3,10 +3,10 @@ const numero1 = document.querySelector('#numero1')
 const numero2 = document.querySelector('#numero2')
 const btnCalcular = document.querySelector('#btn-calcular')
 const resultado = document.querySelector('#resultado')
-const divisao = document.querySelector('#divicao')
-const adicao = document.querySelector('#adicao')
-const subitracao = document.querySelector('#subitracao')
-const multiplicacao = document.querySelector('#multiplicacao')
+const divisao = document.querySelector('.divicao')
+const adicao = document.querySelector('.adicao')
+const subitracao = document.querySelector('.subitracao')
+const multiplicacao = document.querySelector('.multiplicacao')
 // alert(numero1)
 //CRIA FUNÇÃO CALCULAR
 function calcular(){
@@ -14,14 +14,16 @@ function calcular(){
     const n2 = Number (numero2.value)
     if(typeof n1 === 'number'
         && typeof n2 === 'number') {
-            const adicao = `A soma de ${n1} e ${n2} = ${n1=n2}`
-            alert (adicao)
+            const add = `A soma de ${n1} e ${n2} = ${n1+n2}`
+            //alert (add)
+            adicao.innerHTML = add
         } else {
             alert ('Por favor, digite um número')
         }
     //alert('resultado:  ' + resultado)
 }
 //ADICIONA ESCUTAR DE EVENTOS
-btnCalcular.addEventListener('click', function(){
+btnCalcular.addEventListener('click', function(evento){
+   evento.preventDefault() // Tirar o comportamento padrão
    calcular()
 })
