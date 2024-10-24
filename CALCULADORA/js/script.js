3cd50f87f8cc5621f7a6a77d5ac10d8a18e84175
Guto-potato <gustavo.borghi18@gmail.com>
2024-10-24 14:55:16 +0000

    rarrumando a calculadora legal

--- CALCULADORA/js/script.js
+++ CALCULADORA/js/script.js
@@ -3,7 +3,7 @@ const numero1 = document.querySelector('#numero1')
 const numero2 = document.querySelector('#numero2')
 const btnCalcular = document.querySelector('#btn-calcular')
 const resultado = document.querySelector('#resultado')
-const divisao = document.querySelector('.divicao')
+const divisao = document.querySelector('.divisao')
 const adicao = document.querySelector('.adicao')
 const subitracao = document.querySelector('.subitracao')
 const multiplicacao = document.querySelector('.multiplicacao')
@@ -15,8 +15,14 @@ function calcular(){
     if(typeof n1 === 'number'
         && typeof n2 === 'number') {
             const add = `A soma de ${n1} e ${n2} = ${n1+n2}`
+            const sub = `A subtração de ${n1} e ${n2} = ${n1-n2}`
+            const div = `A divisão de ${n1} e ${n2} = ${n1/n2}`
+            const mult = `A multiplicação de ${n1} e ${n2} = ${n1*n2}`
             //alert (add)
             adicao.innerHTML = add
+            subitracao.innerHTML = sub
+            divisao.innerHTML = div
+            multiplicacao.innerHTML = mult
         } else {
             alert ('Por favor, digite um número')
         }
@@ -24,6 +30,6 @@ function calcular(){
 }
 //ADICIONA ESCUTAR DE EVENTOS
 btnCalcular.addEventListener('click', function(evento){
-   evento.preventDefault() // Tirar o comportamento padrão
+   evento.preventDefault()
    calcular()
 })
